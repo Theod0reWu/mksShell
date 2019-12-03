d332640053d7565bc8c971ef7671d93bf244bf98
VictoriaSzy <vszymanowski00@stuy.edu>
2019-12-03 15:23:01 -0500

    Added parsing with a space and directions

---
+++ shell.c
@@ -0,0 +1,56 @@
+/*
+You (and a partner if desired - highly suggested) are to write a shell.
+The shell should implement as many of these features as possible.
+The first being the most basic up to the last which is the most complex.
+1. Read a line at a time, parse the line to separate the command from its arguments.
+   It should then fork and exec the command. The parent process should wait until
+   the exec'd program exits and then it should read the next command.
+   Note: exit and cd cannot be run through a forked child process, you will have to implement these commands on your own.
+         check out the chdir() function
+2. Read and separate multiple commands on one line with ;.
+   That is, the line ls -l ; echo hello should first run the command ls -l and then echo hello.
+3. Implement simple redirection using > (redirecting stdout) and < (redirecting stdin).
+4. Implement simple pipes, that is: ls | wc would run ls and use the output from ls as the input for wc.
+   Check out popen() for this.
+
+All of these commands work in bash, so use it to check out the functionality of each feature if you are unclear about anything.
+
+Specific requirements
+1. Every function you write must have a function header describing the arguments, return value and what the function does.
+  - Use modular design liberally. This is a large program.
+2. You should have a readme file with the following:
+  - A description of what features your shell implements
+  - A description of what features you attempted to implement but were unsuccessful
+  - Any bugs or things you want me to know about your program
+  - A copy of every function header
+  - An example of the readme file can be found in the github repository for this assignment
+3. This project should use separate C files as necessary.
+4. The project should be built using make and a makefile, I will only compile by using make.
+
+Try starting with these restrictions on input:
+- To make your life easier, you could require that all items on the command line are to be separated by a single space.
+- When parsing the line, it might help to count the number of tokens (individual pieces), before parsing.
+- You can restrict the locations and ordering of > and <.
+- You can limit piping (|) to a single pipe.
+*/
+
+/*
+char ** parse_args(char * line) ;
+The line argument is the command typed into the shell.
+It seperates this command at the spaces and puts each part into char * a
+returns char * a
+ */
+char ** parse_args(char * line) {
+  char ** a = calloc(256, sizeof(char *)) ; // initialize the array that will hold the args
+  int i = 0 ;
+  char * q = strsep(&line, " ") ;
+  while (q != NULL) {
+    if (strcmp(q, "")) {
+      // compare the current char entered into the shell to "" to see if it's empty
+      a[i] = q ;
+      i++ ;
+    }
+    q = strsep(&line, " ") ;
+  }
+  return a ;
+}
