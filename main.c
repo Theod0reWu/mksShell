af2dfcd7819ab8268cae4b3cad929f031e23baf8
VictoriaSzy <vszymanowski00@stuy.edu>
2019-12-04 15:16:10 -0500

    Attempting to use scanf

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 
 int main() {
   char dir[256] ;
+  char line[256] ;
   int i ;
   char ** lofParseSpace ;
   char line[256];
   while (1) {
     // INFINITE LOOP!!
     printf("%s$ ", getcwd(dir, sizeof(dir)) ) ; // prints out current path - where we at
+    scanf("%[^\n]", line) ;
 
     fgets(line, 256, stdin) ; // transfer input to line
 
