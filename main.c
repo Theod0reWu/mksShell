bac26818a4816760db97411e3cc72cf0099df5bf
Theodore Wu <twu00@stuy.edu>
2019-12-04 15:02:35 -0500

    stuff

--- main.c
+++ main.c
@@ -4,21 +4,20 @@ int main() {
   char dir[256] ;
   int i ;
   char ** lofParseSpace ;
+  char line[256];
   while (1) {
     // INFINITE LOOP!!
-    printf("%s$ \n", getcwd(dir, sizeof(dir)) ) ; // prints out current path - where we at
+    printf("%s$ ", getcwd(dir, sizeof(dir)) ) ; // prints out current path - where we at
 
-    char * line = malloc(256) ;
     fgets(line, 256, stdin) ; // transfer input to line
-    char ** args = parse_args_semicolon(line) ; // let's see what our command is
 
-    for (i = 0 ; i < 256 ; i++) {
-      if (args[i] != NULL) {
-        lofParseSpace = parse_args_space(args[i]) ;
-      }
+    if (strcmp(line, "exit")){
+      return 0;
     }
+    printf("%s\n", line);
+
     strcpy(line, "");
   }
-  
+
   return 0 ;
 }
