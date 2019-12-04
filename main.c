639405a5b1ae9ef5df7ceafaa23712fb714dee6f
Theodore Wu <twu00@stuy.edu>
2019-12-04 15:15:19 -0500

    d

--- main.c
+++ main.c
@@ -7,13 +7,19 @@ int main() {
   char line[256];
   while (1) {
     // INFINITE LOOP!!
-    printf("%s$ ", getcwd(dir, sizeof(dir)) ) ; // prints out current path - where we at
-
+    //printf("%s$ ", getcwd(dir, sizeof(dir)) ) ; // prints out current path - where we at
+    printf("HELLO:") ;
     fgets(line, 256, stdin) ; // transfer input to line
+    *strchr(line, '\n') = '\0';
 
-    if (strcmp(line, "exit")){
+    //printf("%s\n", line);
+    //printf("%i\n", strcmp(line, "exit"));
+    if (strcmp(line, "exit") == 0){
+      //printf("exiting\n");
       return 0;
     }
+    else if (){}
+    else{}
     printf("%s\n", line);
 
     strcpy(line, "");
