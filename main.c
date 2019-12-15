48f096b722fdc352ef2ca278e3a290d4bf0098d0
VictoriaSzy <vszymanowski00@stuy.edu>
2019-12-15 18:52:04 -0500

    Added detection of piping and removed unneccessary comments

--- main.c
+++ main.c
@@ -8,32 +8,29 @@ int main() {
   char ** args;
   int parent_pid = getpid();
   while (1) {
-    printf("Parent id: %d\n", getppid()) ;
-
     // INFINITE LOOP!!
     //printf("%i\n", fileno(stdin));
     printf("%s$ ", getcwd(dir, sizeof(dir)) ) ; // prints out current path - where we at
     fgets(line, 256, stdin) ; // transfer input to line
-    //printf("uh oh\n");
-
     //printf("|%s|\n", line);
 
     *strchr(line, '\n') = '\0';
     commands = parse_args_semicolon(line) ; // these are all of the commands the user entered into the shell
 
-      //printf("%s\n", line);
-      //printf("%i\n", strcmp(line, "exit"));
+    //printf("%s\n", line);
+    //printf("%i\n", strcmp(line, "exit"));
     i = 0 ;
-    char commandcopy[256];
+    char commandcopy[256] ;
     while (commands[i] != NULL) {
-      strcpy(commandcopy, commands[i]);
+      strcpy(commandcopy, commands[i]) ;
       args = parse_args_space(commandcopy) ; // now the input has been separated by spaces
 
       if (strcmp(args[0], "exit") == 0){
         //printf("exiting\n");
-        printf("Parent id: %d\n", getppid()) ;
+        //printf("Parent id: %d\n", getppid()) ;
         return 0;
       }
+
       else if (strcmp(args[0], "cd") == 0) {
         // we need to change the directory to what the user typed in
         //printf("|%s|\n", args[1]);
@@ -45,7 +42,8 @@ int main() {
           chdir(getpwuid(getuid())->pw_dir) ;
         }
       }
-      else{
+
+      else {
         //printf("%s\n",commands[i] );
         //printf("%i\n",is_redirect(commands[i]) );
         int w = 0;
@@ -54,11 +52,8 @@ int main() {
           if (redirecting(commands[i]) == -1) return -1;
           w = 1;
         }
-        else if(strchr(commands[i],'|')) {
-          if (pipe_it_up(commands[i]) == -1) {
-            printf("At least one of the commands entered is invalid\n") ;
-            return -1 ;
-          }
+        else if (strchr(commands[i], '|') != NULL) {
+          pipe_it_up(commands[i]) ;
         }
         else if (execute(args) != 0) {
           return -1; //if the child did not execute properly, kill the child process
