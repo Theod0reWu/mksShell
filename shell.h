54ce34d20e8ab4add690968a88a7a452ec7c5e3f
VictoriaSzy <vszymanowski00@stuy.edu>
2019-12-15 21:41:40 -0500

    Checked function headers

--- shell.h
+++ shell.h
@@ -2,24 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 #include <string.h>
 #include <errno.h>
-
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <unistd.h>
 #include <dirent.h>
-
 #include <math.h>
-
 #include <signal.h>
 
-char ** parse_args_space(char * line) ;
-char ** parse_args_semicolon(char * line) ;
-int redirecting(char * line) ;
-int execute(char ** command) ;
 char ** parse_args(char * line, char * del) ;
+char ** parse_args_semicolon(char * line) ;
+char ** parse_args_space(char * line) ;
 int is_redirect(char * line) ;
+int redirecting(char * line) ;
 int pipe_it_up(char * c) ;
+int execute(char ** args) ;
